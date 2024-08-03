47b2cc8e246a63012254eebc3cc3eb29856def7b
Grady Schofield <gradyschofield@protonmail.com>
2024-08-03 18:19:15 -0500

    save stuff

--- src/MMapViewableOpenHashMap.hpp
+++ src/MMapViewableOpenHashMap.hpp
@@ -27,10 +27,31 @@ SOFTWARE.
 
 #include<functional>
 #include<fstream>
+#include<string>
 
 #include<OpenHashMap.hpp>
 
 namespace gradylib {
+    template<typename ValueType>
+    concept serializable_global = requires (std::ofstream & os, ValueType const & v) {
+        serialize(os, v);
+    };
+
+    template<typename ValueType>
+    concept serializable_method = requires (std::ofstream & os, ValueType const & v) {
+        { v.serialize(os) } -> std::same_as<void>;
+    };
+
+    template<typename ValueType>
+    concept viewable_global = requires (std::byte const * ptr) {
+        { makeView<ValueType>(ptr) } -> std::same_as<std::span<ValueType const>>;
+    };
+
+    template<typename ValueType>
+    concept viewable_method = requires (std::byte const * ptr) {
+        { ValueType::makeView(ptr) } -> std::same_as<std::span<ValueType const>>;
+    };
+
     template<typename Key, typename Value, template<typename> typename HashFunction = std::hash>
     class MMapViewableOpenHashMap {
         HashFunction<Key> hashFunction = HashFunction<Key>{};
@@ -42,10 +63,15 @@ namespace gradylib {
 
         public:
             template<typename KeyType, typename ValueType>
-            requires (std::ofstream & os, ValueType & v) {
-                { serialize(os, v) } -> std::same_as<void>;
-            }
+            requires (serializable_global<ValueType> || serializable_method<ValueType>) &&
+                (viewable_global<ValueType> || viewable_method<ValueType>) &&
+                (std::is_same_v<std::remove_reference_t<KeyType>, Key> || std::is_convertible_v<std::remove_reference_t<KeyType>, Key>) &&
+                std::is_same_v<std::remove_const_t<std::remove_reference_t<ValueType>>, Value>
             void put(KeyType && key, ValueType && value) {
+                m.put(std::forward<KeyType>(key), std::forward<ValueType>(value));
+            }
+
+            void write(std::string filename) {
 
             }
         };
