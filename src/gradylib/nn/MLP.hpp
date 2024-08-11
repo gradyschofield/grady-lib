da536f5f67c6038351ac4c09cc951bd27e7e75d7
Grady Schofield <grady.schofield@gmail.com>
2024-08-10 22:15:28 -0500

    ideas for NN builder

---
+++ src/gradylib/nn/MLP.hpp
@@ -0,0 +1,66 @@
+//
+// Created by Grady Schofield on 8/10/24.
+//
+
+#ifndef GRADY_LIB_MLP_HPP
+#define GRADY_LIB_MLP_HPP
+
+#include<gradylib/nn/Node.hpp>
+
+namespace gradylib {
+    namespace nn {
+
+        template<typename WeightType = float, typename BiasType = WeightType>
+        class MLP : public Node {
+
+            class MLPImpl : public NodeImpl {
+                int numInputs;
+                int numOutputs;
+                std::shared_ptr<NodeImpl const> input;
+
+            public:
+                MLPImpl(int numInputs, int numOutputs)
+                    : numInputs(numInputs), numOutputs(numOutputs)
+                {
+                }
+
+                MLPImpl(Node const & input, int numOutputs)
+                    : numInputs(input.getNumOutputs()), numOutputs(numOutputs), input(getImpl(input))
+                {
+                }
+
+                int getNumOutputs() const override {
+                    return numOutputs;
+                }
+            };
+
+            std::shared_ptr<MLPImpl> impl;
+
+            std::shared_ptr<NodeImpl> getImpl() const override {
+                return impl;
+            }
+
+        public:
+
+            typedef WeightType weight_type;
+            typedef BiasType bias_type;
+
+            MLP(int numInputs, int numOutputs)
+                : impl(std::make_shared<MLPImpl>(numInputs, numOutputs))
+            {
+            }
+
+            MLP(Node const & input, int numOutputs)
+                : impl(std::make_shared<MLPImpl>(input, numOutputs))
+            {
+            }
+
+            int getNumOutputs() const override {
+                return impl->getNumOutputs();
+            }
+        };
+
+    }
+}
+
+#endif //GRADY_LIB_MLP_HPP
---
+++ src/gradylib/nn/Node.hpp
@@ -0,0 +1,54 @@
+//
+// Created by Grady Schofield on 8/10/24.
+//
+
+#ifndef GRADY_LIB_NODE_HPP
+#define GRADY_LIB_NODE_HPP
+
+#include<concepts>
+#include<memory>
+#include<vector>
+
+namespace gradylib {
+    namespace nn {
+#if 0
+        template<typename T>
+        concept Node = requires(T n) {
+            { n.getNumOutputs() } -> std::convertible_to<int>;
+        };
+#else
+        class NodeImpl;
+
+        class Node {
+            virtual std::shared_ptr<NodeImpl> getImpl() const = 0;
+        public:
+            virtual int getNumOutputs() const = 0;
+            virtual ~Node() = default;
+
+            friend NodeImpl;
+        };
+
+        class NodeImpl {
+        protected:
+            std::shared_ptr<NodeImpl> getImpl(Node const & n) {
+                return n.getImpl();
+            }
+        public:
+            virtual int getNumOutputs() const = 0;
+            virtual ~NodeImpl() = default;
+        };
+
+
+        template<typename T>
+        concept IndexGenerator = requires(T n) {
+            { n() } -> std::same_as<std::vector<int>>;
+            std::is_base_of_v<Node, T>;
+        };
+
+#endif
+
+        //typedef std::variant<MLP> Node;
+    }
+}
+
+#endif //GRADY_LIB_NODE_HPP
