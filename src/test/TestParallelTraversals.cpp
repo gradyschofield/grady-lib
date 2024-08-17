51c8aa1b981a3a5ba2a51855f516a971eeb8bf1f
Grady Schofield <grady.schofield@gmail.com>
2024-08-17 13:52:16 -0500

    get started with Catch2 v3

--- src/test/TestParallelTraversals.cpp
+++ src/test/TestParallelTraversals.cpp
@@ -2,13 +2,15 @@
 // Created by Grady Schofield on 7/27/24.
 //
 
+#include<catch2/catch_test_macros.hpp>
+
 #include"gradylib/AltIntHash.hpp"
 #include"gradylib/OpenHashMap.hpp"
 
 using namespace gradylib;
 using namespace std;
 
-int main() {
+TEST_CASE() {
     OpenHashMap<int64_t, int64_t, AltIntHash> m;
     size_t num = 100000;
     for (size_t i = 0; i < num; ++i) {
@@ -34,5 +36,5 @@ int main() {
     auto endTime = chrono::high_resolution_clock::now();
     cout << "time: " << chrono::duration_cast<chrono::milliseconds>(endTime - startTime).count() << " ms\n";
     std::cout << r.size() << " " << m.size() << "\n";
-    return 0;
+    REQUIRE( r.size() == m.size());
 }
